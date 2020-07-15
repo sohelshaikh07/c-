2b9eb143d36d1b31d7ad94ceb4e91a83953cbf49
sohel shaikh <67818886+sohelshaikh07@users.noreply.github.com>
2020-07-15 07:39:21 +0530

    Add files via upload
    V-Table Analyzer

---
+++ vtablean.cpp
@@ -0,0 +1,60 @@
+//Vtable Analyzer
+
+using namespace std;
+
+#include<iostream>
+
+class  Base
+{
+    public:
+    int a,b;
+        virtual void fun()
+        {
+            cout<<"Base Fun\n";
+        }
+        virtual void gun()
+        {
+            cout<<"Base Gun\n";
+        }
+        void sun()
+        {
+            cout<<"Base Sun\n";
+        }
+};
+
+class Derived:public Base
+{
+    public:
+       int x,y; 
+        void fun()
+        {
+            cout<<"Derived Fun\n";
+        }
+        void run()
+        {
+            cout<<"Derived run\n";
+        }
+        virtual void sun()
+        {
+            cout<<"Derived Sun\n";
+        }
+};
+
+int main()
+{
+    Base boj;
+    Derived dobj;
+    int *op=(int*)&dobj;
+    int *vtable=(int *)*op;
+    void (*fp)();
+    fp=(void (*)())*vtable;
+    fp();
+
+    fp=(void (*)())*(vtable+1);
+    fp();
+    fp=(void (*)())*(vtable+2);
+    fp();
+    fp=(void (*)())*(vtable+0);
+    fp();
+    return 0;
+}
